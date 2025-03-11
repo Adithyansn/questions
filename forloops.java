101d587f143c0afd9f9032760426852467e74a2e
Adithyansn <snadithyan7@gmail.com>
2025-03-11 15:10:46 +0530

    Add files via upload

---
+++ forloops.java
@@ -0,0 +1,8 @@
+public class forloops {
+    public static void main(String[] args) {
+        // using for loop to print numbers from 10 to 50
+        for (int i = 10; i <=50; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop {
+    public static void main(String[] args) {
+        // using while loop to print numbers from 10 to 50
+     int i = 10;
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+
+    }
+}}
+
